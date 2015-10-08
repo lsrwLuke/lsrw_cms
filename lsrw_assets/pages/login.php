7e29f719f5e4968262581b20f66d6587cbf61b6b
lsrwLuke <luke@lsrw.co.uk>
2015-10-08 21:28:21 +0100

    Removing unused files

--- lsrw_assets/pages/login.php
+++
@@ -1,6 +0,0 @@
-<form method="POST" action="<?php echo root( false ) . '?page=panel_home'; ?>">
-	<input type="hidden" name="action" value="login"/>
-	<input type="text" name="username" id="username"/>
-	<input type="password" name="password" id="password"/>
-	<input type="submit" value="Login"/>
-</form>
--- lsrw_assets/pages/panel_home.php
+++
@@ -1,5 +0,0 @@
-<h1>Todo List:</h1>
-<ul>
-	<li>Finish creating each page on sidebar</li>
-	<li>Finish creating homepage.</li>
-</ul>
